6cfc391eaf5402f05ea13d0c3a9a7177f8330935
Đào Duy Tùng <duytung.developer@gmail.com>
2024-12-21 04:47:20 +0700

    update queue

--- src/queue.c
+++ src/queue.c
@@ -9,23 +9,35 @@ int empty(struct queue_t * q) {
 }
 
 void enqueue(struct queue_t *q, struct pcb_t *proc) {
-    if (q->size >= MAX_QUEUE_SIZE) return; // Hàng đợi đầy
-    int i = q->size - 1;
-    while (i >= 0 && q->proc[i]->priority > proc->priority) {
-        q->proc[i + 1] = q->proc[i]; // Dịch process có độ ưu tiên thấp hơn
-        i--;
-    }
-    q->proc[i + 1] = proc; // Thêm process vào đúng vị trí
-    q->size++;
+    /* TODO: put a new process to queue [q] */
+	if (q->size >= MAX_QUEUE_SIZE)
+		return; // Hàng đợi đầy
+	int i = q->size - 1;
+	#ifdef MLQ_SCHED
+	while (i >= 0 && q->proc[i]->prio > proc->prio)
+	#else
+	while (i >= 0 && q->proc[i]->priority > proc->priority)
+	#endif
+	{
+		q->proc[i + 1] = q->proc[i]; // Dịch process có độ ưu tiên thấp hơn
+		i--;
+	}
+	q->proc[i + 1] = proc; // Thêm process vào đúng vị trí
+	q->size++;
 }
 
 struct pcb_t *dequeue(struct queue_t *q) {
-    if (q->size == 0) return NULL; // Hàng đợi rỗng
-    struct pcb_t *proc = q->proc[0]; // Lấy process đầu tiên (ưu tiên cao nhất)
-    for (int i = 0; i < q->size - 1; i++) {
-        q->proc[i] = q->proc[i + 1]; // Dịch các process còn lại lên
+    /* TODO: return a pcb whose prioprity is the highest
+	 * in the queue [q] and remember to remove it from q
+	 * */
+	if (q->size == 0)
+		return NULL;				 // Hàng đợi rỗng
+	struct pcb_t *proc = q->proc[0]; // Lấy process đầu tiên (ưu tiên cao nhất)
+	for (int i = 0; i < q->size - 1; i++)
+	{
+		q->proc[i] = q->proc[i + 1]; // Dịch các process còn lại lên
 	}
-    q->size--;
-    return proc;
+	q->size--;
+	return proc;
 }
 
